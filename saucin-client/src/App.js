81f952c658a22f7c656beafd8e5a27c9f9f69633
Alexinder Virdee <alex.virdee08@gmail.com>
2020-03-04 11:29:18 -0500

    Setup redux store

--- saucin-client/src/App.js
+++ saucin-client/src/App.js
@@ -5,22 +5,29 @@ import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Signup from './components/auth/Signup';
 import Footer from './components/layout/Footer';
+
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 import './App.css';
 
 const App = () => (
-  <Router>
-    <Fragment>
-      <Navbar />
-      <Route exact path="/" component={Landing} />
-      <section>
-        <Switch>
-          <Route exact path="/signup" component={Signup} />
-          <Route exact path="/login" component={Login} />
-        </Switch>
-      </section>
-      <Route exact path="/" component={Footer} />
-    </Fragment>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <section>
+          <Switch>
+            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+        </section>
+        <Route exact path="/" component={Footer} />
+      </Fragment>
+    </Router>
+  </Provider>
 );
 
 export default App;
