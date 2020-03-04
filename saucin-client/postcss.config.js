92dab098175dda9238d2a98461139d41cbeee6b9
Alexinder Virdee <alex.virdee08@gmail.com>
2020-03-03 19:26:42 -0500

    Purge css remove utilities not being used

--- saucin-client/postcss.config.js
+++ saucin-client/postcss.config.js
@@ -1,4 +1,8 @@
 const tailwindcss = require('tailwindcss');
 module.exports = {
-  plugins: [tailwindcss('./tailwind.js'), require('autoprefixer')]
+  plugins: [
+    tailwindcss('./tailwind.js'),
+    require('autoprefixer'),
+    ...(process.env.NODE_ENV === 'production' ? [purgecss] : [])
+  ]
 };
