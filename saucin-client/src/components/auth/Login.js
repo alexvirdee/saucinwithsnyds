b6ca455536b063bb31348160906bc2495136653f
Alexinder Virdee <alex.virdee08@gmail.com>
2020-03-04 10:52:04 -0500

    Register form data from react working

--- saucin-client/src/components/auth/Login.js
+++ saucin-client/src/components/auth/Login.js
@@ -5,39 +5,39 @@ const Login = () => {
   return (
     <div className="flex justify-center lg:pt-6 h-screen lg:bg-gray-700 sm:pt-0 xs:bg-white">
       <div>
-        <form class="p-12 m-1 bg-white rounded sm:shadow-none lg:shadow">
+        <form className="p-12 m-1 bg-white rounded sm:shadow-none lg:shadow">
           <div className="text-4xl pb-4">Login to Saucinwithsnyds</div>
-          <div class="flex flex-wrap -mx-3">
-            <div class="w-full px-3 md:mb-0">
+          <div className="flex flex-wrap -mx-3">
+            <div className="w-full px-3 md:mb-0">
               <label
-                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                for="grid-first-name"
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="grid-first-name"
               >
                 Email
               </label>
               <input
-                class="appearance-none block w-full bg-gray-200 text-gray-700 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                className="appearance-none block w-full bg-gray-200 text-gray-700 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
                 id="grid-first-name"
                 type="text"
                 placeholder="Jane@example.com"
               ></input>
             </div>
           </div>
-          <div class="flex flex-wrap -mx-3 mb-6">
-            <div class="w-full px-3">
+          <div className="flex flex-wrap -mx-3 mb-6">
+            <div className="w-full px-3">
               <label
-                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                for="grid-password"
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="grid-password"
               >
                 Password
               </label>
               <input
-                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                 id="grid-password"
                 type="password"
                 placeholder="******************"
               ></input>
-              <p class="text-gray-600 text-xs italic">
+              <p className="text-gray-600 text-xs italic">
                 Password must be 6 characters or longer
               </p>
             </div>
@@ -48,6 +48,14 @@ const Login = () => {
           >
             Login
           </Link>
+          <div className="ml-4 px-3 text-sm italic inline">
+            <p className="inline">
+              Don't have an account?
+              <Link className="text-blue-400 pl-2" to="/signup">
+                Register
+              </Link>
+            </p>
+          </div>
         </form>
       </div>
     </div>
--- saucin-client/src/components/auth/Signup.js
+++ saucin-client/src/components/auth/Signup.js
@@ -1,80 +1,142 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
 import { Link } from 'react-router-dom';
 
-import bgImage from '../../img/grill-chicken.jpg';
-
 const Signup = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+  });
+
+  const { name, email, password, password2 } = formData;
+
+  const onChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = e => {
+    e.preventDefault();
+    if (password !== password2) {
+      console.log('Passwords do not match.');
+    } else {
+      console.log(formData);
+    }
+  };
+
   return (
-    <div className="flex justify-center lg:pt-6 h-screen lg:bg-gray-700 sm:pt-0 xs:bg-white">
-      <div>
-        <form class="p-12 m-1 bg-white rounded sm:shadow-none lg:shadow">
-          <div className="text-4xl pb-4">Create your account today</div>
-          <div class="flex flex-wrap -mx-3 mb-2">
-            <div class="w-full px-3 md:mb-0">
-              <label
-                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                for="grid-first-name"
-              >
-                Username
-              </label>
-              <input
-                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                id="grid-first-name"
-                type="text"
-                placeholder="Jane"
-              ></input>
-              <p class="text-red-500 text-xs italic">
-                Please fill out this field.
-              </p>
-            </div>
-          </div>
-          <div class="flex flex-wrap -mx-3">
-            <div class="w-full px-3 md:mb-0">
-              <label
-                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                for="grid-first-name"
-              >
-                Email
-              </label>
-              <input
-                class="appearance-none block w-full bg-gray-200 text-gray-700 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                id="grid-first-name"
-                type="text"
-                placeholder="Jane@example.com"
-              ></input>
-            </div>
-          </div>
-          <div class="flex flex-wrap -mx-3 mb-6">
-            <div class="w-full px-3">
-              <label
-                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                for="grid-password"
-              >
-                Password
-              </label>
-              <input
-                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                id="grid-password"
-                type="password"
-                placeholder="******************"
-              ></input>
-              <p class="text-gray-600 text-xs italic">
-                Password must be 6 characters or longer
-              </p>
-            </div>
-          </div>
-          <Link
-            to="/signup"
-            className="text-center bg-green-600 hover:bg-green-500 text-white font-bold py-2 px-4 rounded shadow rounded shadow"
+    <Fragment>
+      <div className="flex justify-center lg:pt-6 h-screen lg:bg-gray-700 sm:pt-0 xs:bg-white">
+        <div>
+          <form
+            onSubmit={e => onSubmit(e)}
+            className="p-12 m-1 bg-white rounded sm:shadow-none lg:shadow"
           >
-            Sign up
-          </Link>
-          <div className="ml-4 px-3 text-blue-400 text-sm italic inline">
-            <Link to="/login">Already have an account? Login</Link>
-          </div>
-        </form>
+            <div className="text-4xl pb-4">Create your account today</div>
+            <div className="flex flex-wrap -mx-3 mb-2">
+              <div className="w-full px-3 md:mb-0">
+                <label
+                  className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                  htmlFor="grid-first-name"
+                >
+                  Username
+                </label>
+                <input
+                  className="appearance-none block w-full bg-gray-200 text-gray-700 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                  id="grid-first-name"
+                  type="text"
+                  value={name}
+                  onChange={e => onChange(e)}
+                  name="name"
+                  placeholder="Jane"
+                  required
+                ></input>
+                {/* <p className="text-red-500 text-xs italic">
+                  Please fill out this field.
+                </p> */}
+              </div>
+            </div>
+            <div className="flex flex-wrap -mx-3">
+              <div className="w-full px-3 md:mb-0">
+                <label
+                  className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                  htmlFor="grid-first-name"
+                >
+                  Email
+                </label>
+                <input
+                  className="appearance-none block w-full bg-gray-200 text-gray-700 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                  id="email"
+                  type="text"
+                  name="email"
+                  value={email}
+                  onChange={e => onChange(e)}
+                  placeholder="Jane@example.com"
+                  required
+                ></input>
+              </div>
+            </div>
+            <div className="flex flex-wrap -mx-3 mb-6">
+              <div className="w-full px-3">
+                <label
+                  className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                  htmlFor="grid-password"
+                >
+                  Password
+                </label>
+                <input
+                  className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                  type="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={e => onChange(e)}
+                  name="password"
+                  minLength="6"
+                ></input>
+                {/* <p className="text-gray-600 text-xs italic">
+                  Password must be 6 characters or longer
+                </p> */}
+              </div>
+            </div>
+            <div className="flex flex-wrap -mx-3 mb-6">
+              <div className="w-full px-3">
+                <label
+                  className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                  htmlFor="grid-password2"
+                >
+                  Confirm Password
+                </label>
+                <input
+                  className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                  type="password"
+                  placeholder="Confirm Password"
+                  name="password2"
+                  value={password2}
+                  onChange={e => onChange(e)}
+                  minLength="6"
+                ></input>
+                {/* <p className="text-gray-600 text-xs italic">
+                  Password must be 6 characters or longer
+                </p> */}
+              </div>
+            </div>
+            <button
+              type="submit"
+              className="text-center bg-green-600 hover:bg-green-500 text-white font-bold py-2 px-4 rounded shadow rounded shadow"
+            >
+              Sign up
+            </button>
+            <div className="ml-4 px-3 text-sm italic inline">
+              <p className="inline">
+                Already have an account?{' '}
+                <Link className="text-blue-400" to="/login">
+                  Login
+                </Link>
+              </p>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
+    </Fragment>
   );
 };
 
