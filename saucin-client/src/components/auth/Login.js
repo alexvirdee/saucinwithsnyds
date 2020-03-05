35e3785d2c33156bc2148805fe24b95ea54089b1
Alexinder Virdee <alex.virdee08@gmail.com>
2020-03-05 08:36:51 -0500

    redirect to dashboard after logging in or registering an account

--- saucin-client/src/components/auth/Login.js
+++ saucin-client/src/components/auth/Login.js
@@ -1,65 +1,105 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { Fragment, useState } from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { login } from '../../actions/auth';
 
-const Login = () => {
+const Login = ({ login, isAuthenticated }) => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  });
+
+  const { email, password } = formData;
+
+  const onChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async e => {
+    e.preventDefault();
+    login(email, password);
+  };
+
+  // Redirect if logged in
+  if (isAuthenticated) {
+    return <Redirect to="/dashboard" />;
+  }
+
   return (
-    <div className="flex justify-center lg:pt-6 h-screen lg:bg-gray-700 sm:pt-0 xs:bg-white">
-      <div>
-        <form className="p-12 m-1 bg-white rounded sm:shadow-none lg:shadow">
-          <div className="text-4xl pb-4">Login to Saucinwithsnyds</div>
-          <div className="flex flex-wrap -mx-3">
-            <div className="w-full px-3 md:mb-0">
-              <label
-                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                htmlFor="grid-first-name"
-              >
-                Email
-              </label>
-              <input
-                className="appearance-none block w-full bg-gray-200 text-gray-700 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                id="grid-first-name"
-                type="text"
-                placeholder="Jane@example.com"
-              ></input>
+    <Fragment>
+      <div className="flex justify-center lg:pt-6 h-screen lg:bg-gray-700 sm:pt-0 xs:bg-white">
+        <div>
+          <form
+            onSubmit={e => onSubmit(e)}
+            className="p-12 m-1 bg-white rounded sm:shadow-none lg:shadow"
+          >
+            <div className="text-4xl pb-4">Login to Saucinwithsnyds</div>
+            <div className="flex flex-wrap -mx-3">
+              <div className="w-full px-3 md:mb-0">
+                <label
+                  className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                  htmlFor="grid-first-name"
+                >
+                  Email
+                </label>
+                <input
+                  className="appearance-none block w-full bg-gray-200 text-gray-700 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                  type="email"
+                  name="email"
+                  value={email}
+                  onChange={e => onChange(e)}
+                  placeholder="Jane@example.com"
+                ></input>
+              </div>
             </div>
-          </div>
-          <div className="flex flex-wrap -mx-3 mb-6">
-            <div className="w-full px-3">
-              <label
-                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                htmlFor="grid-password"
-              >
-                Password
-              </label>
-              <input
-                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                id="grid-password"
-                type="password"
-                placeholder="******************"
-              ></input>
-              <p className="text-gray-600 text-xs italic">
-                Password must be 6 characters or longer
+            <div className="flex flex-wrap -mx-3 mb-6">
+              <div className="w-full px-3">
+                <label
+                  className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                  htmlFor="grid-password"
+                >
+                  Password
+                </label>
+                <input
+                  className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                  type="password"
+                  name="password"
+                  minLength="6"
+                  value={password}
+                  onChange={e => onChange(e)}
+                  placeholder="******************"
+                ></input>
+              </div>
+            </div>
+            <button
+              type="submit"
+              value="Login"
+              className="text-center bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 rounded shadow rounded shadow"
+            >
+              Login
+            </button>
+            <div className="ml-4 px-3 text-sm italic inline">
+              <p className="inline">
+                Don't have an account?
+                <Link className="text-blue-400 pl-2" to="/signup">
+                  Register
+                </Link>
               </p>
             </div>
-          </div>
-          <Link
-            to="/login"
-            className="text-center bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 rounded shadow rounded shadow"
-          >
-            Login
-          </Link>
-          <div className="ml-4 px-3 text-sm italic inline">
-            <p className="inline">
-              Don't have an account?
-              <Link className="text-blue-400 pl-2" to="/signup">
-                Register
-              </Link>
-            </p>
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
-    </div>
+    </Fragment>
   );
 };
 
-export default Login;
+Login.propTypes = {
+  login: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
+};
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps, { login })(Login);
