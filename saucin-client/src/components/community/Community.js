ced9562d5bee45776497b7ceb442ecdeac8370d3
Alexinder Virdee <alex.virdee08@gmail.com>
2020-03-09 15:54:10 -0400

    Filtering design elements

--- saucin-client/src/components/community/Community.js
+++ saucin-client/src/components/community/Community.js
@@ -1,14 +1,45 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 
+import Cooking from '../../img/vegetables.jpg';
+import Lifestyle from '../../img/lifestyle.jpg';
+import General from '../../img/general.jpg';
+
 const Community = props => {
   return (
     <Fragment>
-      <div className="h-40 grid grid-cols-4 gap-1">
-        <div className="bg-gray-400">POST 1</div>
-        <div className="bg-gray-400">POST 2</div>
-        <div className="bg-gray-400">POST 3</div>
-        <div className="bg-gray-400">POST 4</div>
+      <div className="search-recipes">
+        <input
+          class="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
+          type="text"
+          placeholder="Search community posts..."
+        ></input>
+      </div>
+      <div className="pt-1 grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-1">
+        <div className="cursor-pointer category-overlay">
+          <Fragment>
+            <img src={Cooking}></img>
+            <div className="content">
+              <div className="recipe-category-text pacifico">Cooking</div>
+            </div>
+          </Fragment>
+        </div>
+        <div className="cursor-pointer category-overlay">
+          <Fragment>
+            <img src={Lifestyle}></img>
+            <div className="content">
+              <div className="recipe-category-text pacifico">Lifestyle</div>
+            </div>
+          </Fragment>
+        </div>
+        <div className="cursor-pointer category-overlay">
+          <Fragment>
+            <img src={General}></img>
+            <div className="content">
+              <div className="recipe-category-text pacifico">General</div>
+            </div>
+          </Fragment>
+        </div>
       </div>
     </Fragment>
   );
